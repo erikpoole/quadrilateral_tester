69f24d5ebea19b15efde9dd29d9ac4360f9b66f6
Erik Poole <Mahkumazahn@gmail.com>
2019-02-02 14:46:33 -0700

    Random test coverage working

--- randomTesting/generateTests.py
+++ randomTesting/generateTests.py
@@ -2,6 +2,40 @@ import os
 import glob
 import random
 
+def clean_old_files():
+    os.chdir("./tests")
+    os.system("rm -f *.profdata")
+    os.system("rm -f *.profdata")
+    os.system("rm -f *.txt")
+
+    os.chdir("../..")
+    os.system("clang++ -std=c++17 -fprofile-instr-generate -fcoverage-mapping main.cpp -o coverage")
+    os.system("rm -f *.profdata")
+    os.system("rm -f *.profraw")
+    os.system("rm -f coverage.txt")
+
+    os.chdir("./randomTesting")
+
+def create_coverage():
+    os.chdir("..")
+    os.system("touch fuzzOutput.profdata")
+    testSet = glob.glob("./randomTesting/tests/*txt")
+
+    for test in testSet:
+        subStringTest = test[:-4]
+        os.system("./coverage < " + subStringTest + ".txt > ./output.txt")
+        os.system("llvm-profdata merge -sparse ./default.profraw -o ./" + subStringTest + ".profdata")
+
+    profDataSet = glob.glob("./randomTesting/tests/*profdata")
+    combinedProfDataString = " ".join(profDataSet)
+
+    os.system("llvm-profdata merge -sparse " + combinedProfDataString + " -o ./randomOutput.profdata")
+    os.system("llvm-cov show ./coverage -instr-profile=./randomOutput.profdata > coverage.txt")
+
+    os.chdir("./randomTesting")
+
+clean_old_files()
+
 for x in range(1, 101):
     filename = "Test" + str(x) + ".txt"
     file = open("./tests/" + filename, "w+")
@@ -11,24 +45,6 @@ for x in range(1, 101):
         file.write(" ")
     file.write(str(random.randint(0, 101)))
 
-os.chdir("..")
-os.system("clang++ -std=c++17 -fprofile-instr-generate -fcoverage-mapping main.cpp -o coverage")
-os.system("rm -f *.profdata")
-os.system("rm -f *.profraw")
 
 
-os.chdir("./randomTesting/tests")
-os.system("rm -f *.profdata")
-os.system("rm -f *.profdata")
-os.chdir("../..")
-
-os.system("touch randomOutput.profdata")
-testSet = glob.glob("./randomTesting/tests/*txt")
-
-for test in testSet:
-    subStringTest = test[:-4]
-    os.system("./coverage < " + subStringTest + ".txt > ./output.txt")
-    os.system("llvm-profdata merge -sparse ./randomOutput.profdata ./default.profraw -o ./randomOutput.profdata")
-
-
-os.system("llvm-cov show ./coverage -instr-profile=./randomOutput.profdata")
+create_coverage()
